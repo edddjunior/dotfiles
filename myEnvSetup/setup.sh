8947a078b2db2a340c5e0ba2e7132b49856e7340
Edi Junior <edsonbergamojunior@gmail.com>
2019-09-25 12:12:20 -0300

    Update setup.sh

--- myEnvSetup/setup.sh
+++ myEnvSetup/setup.sh
@@ -13,23 +13,23 @@ node_version='10.16.3'
 ruby_version='2.6.4'
 rails_version='6.0.0'
 postgresql_version='10'
-postgresql_password='81035810'
+postgresql_password=''
 echo "Ready."
 
 # Update and upgrade
 echo "Updating repositories..........................................................................................................................................................................."
-sudo apt update -y
-sudo apt upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 echo "Ready."
 
 # Curl and Wget
 echo "Installing Curl and Wget..........................................................................................................................................................................."
-sudo apt install curl -y && apt-get install wget -y
+sudo apt-get install curl -y && apt-get install wget -y
 echo "Ready."
 
 # Git and its conf
 echo "Git..........................................................................................................................................................................."
-sudo apt install git -y
+sudo apt-get install git -y
 git config --global user.name "${git_username}"
 git config --global user.email "${git_email}"
 echo "Ready."
@@ -51,12 +51,12 @@ echo "Ready."
 echo "Yarn..........................................................................................................................................................................."
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg |  apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn -y
+sudo apt-get update && sudo apt-get install yarn -y
 echo "Ready."
 
 # Java
 echo "Java..........................................................................................................................................................................."
-sudo apt install openjdk-8-jdk -y
+sudo apt-get install openjdk-8-jdk -y
 echo "Ready."
 
 # RVM
@@ -67,7 +67,7 @@ echo "Ready."
 
 # Ruby
 echo "Ruby..........................................................................................................................................................................."
-rvm install ${ruby_version}
+rvm install "ruby-${ruby_version}"
 rvm --default use ${ruby_version}
 echo "Ready."
 
@@ -79,9 +79,9 @@ echo "Ready."
 # Elasticsearch
 echo "Elasticsearch..........................................................................................................................................................................."
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-sudo apt install apt-transport-https
+sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt update && sudo apt install elasticsearch
+sudo apt-get update && sudo apt-get install elasticsearch
 
 systemctl start elasticsearch.service && systemctl stop elasticsearch.service
 systemctl stop elasticsearch.service
@@ -89,8 +89,8 @@ echo "Ready."
 
 # Postgres
 echo "Postgres..........................................................................................................................................................................."
-sudo apt install postgresql-${postgresql_version} -y
-sudo apt install libpq-dev -y
+sudo apt-get install postgresql-${postgresql_version} -y
+sudo apt-get install libpq-dev -y
 
 sudo sed -e '/^local   all/s/peer/trust/' /etc/postgresql/${postgresql_version}/main/pg_hba.conf
 sudo service postgresql restart
@@ -100,39 +100,45 @@ sed -e '/^local   all/s/trust/md5/' /etc/postgresql/${postgresql_version}/main/p
 sudo service postgresql restart
 echo "Ready."
 
-# Zsh and OhMyZsh
-echo "Zsh & OhMyZsh..........................................................................................................................................................................."
-sudo apt install fonts-powerline
-sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm -rf ~/.zshrc && wget -O ~/.zshrc https://raw.githubusercontent.com/edddjunior/dotfiles/master/myEnvSetup/.zshrc
-echo "Ready."
-
 # Tmux
 echo "Tmux..........................................................................................................................................................................."
-sudo apt install tmux -y
+sudo apt-get install tmux -y
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/edddjunior/dotfiles/master/myEnvSetup/.tmux.conf
-sudo apt install xclip
+sudo apt-get install xclip
 echo "Ready."
 
 # Vim
 echo "Vim............................................................................................................................................................................"
-sudo apt install vim -y && sudo apt install vim-gnome -y
+sudo apt-get install vim -y && sudo apt-get install vim-gnome -y
 curl -L https://bit.ly/janus-bootstrap | bash
 
-git clone https://github.com/jiangmiao/auto-pairs.git ~/.vim/janus/vim/tools/
-git clone https://github.com/yggdroot/indentline.git ~/.vim/janus/vim/tools/
-git clone https://github.com/dyng/ctrlsf.vim.git ~/.vim/janus/vim/tools/
-git clone https://github.com/rking/ag.vim.git ~/.vim/janus/vim/tools/
-git clone https://github.com/kien/ctrlp.vim ~/.vim/janus/vim/tools/
-git clone https://github.com/tpope/vim-rails.git ~/.vim/janus/vim/tools/
-git clone https://github.com/tpope/vim-ragtag.git ~/.vim/janus/vim/tools/
+cd ~/.vim/janus/vim/tools/
 
-sudo apt install silversearcher-ag -y
+git clone https://github.com/jiangmiao/auto-pairs.git
+git clone https://github.com/yggdroot/indentline.git
+git clone https://github.com/dyng/ctrlsf.vim.git
+git clone https://github.com/rking/ag.vim.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/tpope/vim-rails.git
+git clone https://github.com/tpope/vim-ragtag.git
+sudo apt-get install silversearcher-ag -y
 
 rm -rf ~/.vimrc && wget -O ~/.vimrc https://raw.githubusercontent.com/edddjunior/dotfiles/master/myEnvSetup/.vimrc
 wget -O ~/.vimrc.after https://raw.githubusercontent.com/edddjunior/dotfiles/master/myEnvSetup/.vimrc.after
 wget -O ~/.vimrc.before https://raw.githubusercontent.com/edddjunior/dotfiles/master/myEnvSetup/.vimrc.before
 echo "Ready."
+
+# Zsh and OhMyZsh
+echo "Zsh & OhMyZsh..........................................................................................................................................................................."
+cd ~
+sudo apt-get install fonts-powerline
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm -rf ~/.zshrc && wget -O ~/.zshrc https://raw.githubusercontent.com/edddjunior/dotfiles/master/myEnvSetup/.zshrc
+echo "Ready."
+
+# TRIM SSD
+echo "TRIM SSD........................................................................................................................................................................."
+sudo fstrim -v /
 
 echo "Finished!"
