565fb47b2742b26955d81f1795e6bc0fc655914b
Edi Junior <edsonbergamojunior@gmail.com>
2019-11-28 00:03:08 -0300

    Update setup.sh

--- myEnvSetup/setup.sh
+++ myEnvSetup/setup.sh
@@ -44,10 +44,12 @@ echo "Ready."
 
 # NVM
 echo "NVM... ###############################################################################################################################################################"
-curl -o- https://raw.github.com/nvm-sh/nvm/blob/master/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+mkdir ~/.nvm
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
 echo "Ready."
 
 # Node.js
